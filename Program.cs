02c1288bc1b4011951c36bd6e58f066a30abdbc9
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-03-03 09:46:43 +0200

    add corrected Program.cs

--- Program.cs
+++ Program.cs
@@ -6,18 +6,29 @@ class WordCountingWithSortedDictionary
 {
     static void Main()
     {
-        IDictionary<int, string> numberNames = new Dictionary<int, string>();
+        //IDictionary<int, string> numberNames = new Dictionary<int, string>();
 
-        numberNames.Add(1, "One");  //Adding key/value using Add method
-        numberNames.Add(2, "Two");
-        numberNames.Add(3, "Three");
+        //numberNames.Add(1, "One");  //Adding key/value using Add method
+        //numberNames.Add(2, "Two");
+        //numberNames.Add(3, "Three");
 
-        //  numberNames.Add(3, "Fourth"); Argument Exception key 3 already added
+        ////  numberNames.Add(3, "Fourth"); Argument Exception key 3 already added
 
-        foreach (KeyValuePair<int, string> kvp in numberNames)
+        //foreach (KeyValuePair<int, string> kvp in numberNames)
+        //{
+        //    Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+        //}
+        // Key can be unique, non repeating , key cant be null, alues can be null or repeating 
+        var cities = new Dictionary<string, string>() 
+        {   ///Dictionary Initializer 
+            {"UK", "London, Mancghester, Birmingahm"},
+            { "USA", "Chicago, New York, Washington" },
+            { "India", "Mumbai, New Delchi, Pune"}          
+        };
+
+        foreach (var kvp in cities)
         {
-            Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+            Console.WriteLine("Key: {0}, Value: {1}", kvp.Key , kvp.Value);
         }
-
     }   
-}
+}   
