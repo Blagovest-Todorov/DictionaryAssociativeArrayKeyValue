698fa8d1d111ea48c17d9ffaf6e35080059d9f9e
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-03-03 09:31:06 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+class WordCountingWithSortedDictionary
+{
+    static void Main()
+    {
+        IDictionary<int, string> numberNames = new Dictionary<int, string>();
+
+        numberNames.Add(1, "One");  //Adding key/value using Add method
+        numberNames.Add(2, "Two");
+        numberNames.Add(3, "Three");
+
+        //  numberNames.Add(3, "Fourth"); Argument Exception key 3 already added
+
+        foreach (KeyValuePair<int, string> kvp in numberNames)
+        {
+            Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+        }
+
+    }   
+}
